408c3eb6271635c1a80d906fa24d1d612cb7fd2b
root <woody38921218@gmail.com>
2024-11-26 00:48:25 +0000

    feat: done infra setup

--- constants/data.ts
+++ constants/data.ts
@@ -123,53 +123,53 @@ export type Product = {
 
 export const navItems: NavItem[] = [
   {
-    title: 'Dashboard',
-    url: '/dashboard/overview',
+    title: 'Handover',
+    url: '/constellation_simulation/',
     icon: 'dashboard',
     isActive: false,
     shortcut: ['d', 'd'],
     items: [] // Empty array as there are no child items for Dashboard
   },
+  // {
+  //   title: 'Employee',
+  //   url: '/dashboard/employee',
+  //   icon: 'user',
+  //   shortcut: ['e', 'e'],
+  //   isActive: false,
+  //   items: [] // No child items
+  // },
   {
-    title: 'Employee',
-    url: '/dashboard/employee',
-    icon: 'user',
-    shortcut: ['e', 'e'],
-    isActive: false,
-    items: [] // No child items
-  },
-  {
-    title: 'Product',
-    url: '/dashboard/product',
+    title: 'Routing',
+    url: '/constellation_simulation/routing',
     icon: 'product',
     shortcut: ['p', 'p'],
     isActive: false,
     items: [] // No child items
   },
-  {
-    title: 'Account',
-    url: '#', // Placeholder as there is no direct link for the parent
-    icon: 'billing',
-    isActive: true,
+  // {
+  //   title: 'Account',
+  //   url: '#', // Placeholder as there is no direct link for the parent
+  //   icon: 'billing',
+  //   isActive: true,
 
-    items: [
-      {
-        title: 'Profile',
-        url: '/dashboard/profile',
-        icon: 'userPen',
-        shortcut: ['m', 'm']
-      },
-      {
-        title: 'Login',
-        shortcut: ['l', 'l'],
-        url: '/',
-        icon: 'login'
-      }
-    ]
-  },
+  //   items: [
+  //     // {
+  //     //   title: 'Profile',
+  //     //   url: '/dashboard/profile',
+  //     //   icon: 'userPen',
+  //     //   shortcut: ['m', 'm']
+  //     // },
+  //     {
+  //       title: 'Login',
+  //       shortcut: ['l', 'l'],
+  //       url: '/',
+  //       icon: 'login'
+  //     }
+  //   ]
+  // },
   {
-    title: 'Kanban',
-    url: '/dashboard/kanban',
+    title: 'ISL',
+    url: '/constellation_simulation/isl',
     icon: 'kanban',
     shortcut: ['k', 'k'],
     isActive: false,
